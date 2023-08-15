c62648a1fb555c51014d443ed58757c41e365609
Reyhan05 <kingreyhan298@gmail.com>
2023-08-15 13:10:13 +0700

    :tada: Initial Commit

---
+++ pages/index.vue
@@ -0,0 +1,120 @@
+<template>
+  <div>
+    <div class="container">
+      <b-table class="mt-5" striped hover :items="items" :fields="fields">
+        <template #cell(#)="data">
+          <button class="btn btn-success" @click="onEditForm(data.item)">Edit</button>
+          <button class="btn btn-danger" @click="onDelete(data.item)">Delete</button>
+        </template>
+      </b-table>
+    </div>
+    <div class="container">
+      <b-form @submit="onSubmit" @reset="onReset">
+        <b-form-group id="input-group-2" label="Your Nama:" label-for="input-2">
+          <b-form-input id="input-2" v-model="form.nama" placeholder="Enter nama" required></b-form-input>
+        </b-form-group>
+
+        <b-form-group id="input-group-2" label="Your umur:" label-for="input-2">
+          <b-form-input id="input-2" v-model="form.umur" placeholder="Enter umur" required></b-form-input>
+        </b-form-group>
+
+        <div class="mt-2">
+          <b-button type="submit" variant="primary">Submit</b-button>
+          <b-button type="reset" variant="danger">Reset</b-button>
+        </div>
+      </b-form>
+      <div v-if="onEdit" class="mt-5">
+        <b-form @submit="onSubmitEdit">
+        <b-form-group id="input-group-2" label="Your nama:" label-for="input-2">
+          <b-form-input id="input-group-2" v-model="form_edit.nama" placeholder="Enter nama" required></b-form-input>
+        </b-form-group>
+        <b-form-group id="input-group-2" label="Your umur:" label-for="input-2">
+          <b-form-input id="input-group-2" v-model="form_edit.umur" placeholder="Enter umur" required></b-form-input>
+        </b-form-group>
+
+        <div class="mt-2">
+          <b-button type="submit" variant="primary">Submit</b-button>
+        </div>
+      </b-form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        form: {
+          nama: "",
+          umur: "",
+        },
+        onEdit: false,
+        fields: [
+          { key: "id", sortable: "true" },
+          { key: "nama", sortable: "true" },
+          { key: "umur", sortable: "true" },
+          { key: "#", sortable: "true" },
+        ],
+        items: [],
+      };
+    },
+    methods: {
+      async onDelete(data) {
+        await this.$axios.$delete(
+          "http://localhost:35000/users/employee" + data.id
+        );
+        this.getapi
+      },
+      async onEditForm(data) {
+        this.onEdit = true;
+        console.log(data);
+        this.form_edit.nama = data.nama;
+        this.form_edit.umur = data.umur;
+        this.form_edit.id = data.id;
+      },
+      async getapi() {
+        const data = await this.$axios.$get(
+          "http://localhost:35000/users/employee",
+        );
+        this.items = data.data;
+        for (let i = 0; i < this.items.length; i++) {
+          const element = this.items[i];
+          element.id = i + 1;
+        }
+      },
+      async onSubmitEdit(event) {
+        event.preventDefault();
+        try {
+          await this.$axios.$put(
+          "http://localhost:35000/users/employee" + this.form_edit.id,
+          this.form_edit
+        );
+        this.getapi
+        } catch(error) {
+          console.log(error)
+        }
+      },
+      async onSubmit(event) {
+        event.preventDefault();
+        await this.$axios.$post(
+          "http://localhost:35000/users/employee",
+          this.form
+        );
+        this.getapi();
+      },
+      onReset(event) {
+        event.preventDefault();
+        this.form.nama = "";
+        this.form.umur = "";
+        this.show = false;
+        this.$nextTick(() => {
+          this.show = true;
+        });
+      },
+    },
+    mounted() {
+      this.getapi();
+    },
+  }
+</script>
---
+++ pages/mobil/index.vue
@@ -0,0 +1,140 @@
+<template>
+    <div>
+    <div class="container border mt-5">
+      <b-table striped hover :items="items" :fields="fields">
+        <template #cell(#)="data">
+            <button class="btn btn-success" @click="onEditForm(data.item)">
+            Edit
+          </button>
+          <button class="btn btn-danger" @click="onDelete(data.item)">
+            Delete
+          </button>
+        </template>
+      </b-table>
+    </div>
+    <div class="container">
+      <b-form @submit="onSubmit" @reset="onReset">
+        <b-form-group id="input-group-1" label="Merek Mobil" label-for="input-1">
+          <b-form-input id="input-1" v-model="form.merek_mobil" placeholder="Masukan Merek mobil" required>
+          </b-form-input>
+        </b-form-group>
+        <b-form-group id="input-group-2" label="Tipe mobil" label-for="input-2">
+          <b-form-input id="input-2" v-model="form.tipe_mobil" placeholder="Masukan tipe mobil" required>
+          </b-form-input>
+        </b-form-group>
+        <b-form-group id="input-group-3" label="Warna Mobil" label-for="input-3">
+          <b-form-input id="input-3" v-model="form.warna" placeholder="Masukan warna mobil" required>
+          </b-form-input>
+        </b-form-group>
+        <b-button type="submit" variant="primary">Submit</b-button>
+        <b-button type="reset" variant="danger">Reset</b-button>
+      </b-form>
+
+      <!-- Form edit -->
+      <div v-if="onEdit" class="mt-5">
+        <b-form @submit="onSubmitEdit">
+          <b-form-group id="input-group-1" label="Barang:" label-for="input-1">
+            <b-form-input v-model="form_edit.merek_mobil" value="form.merek_mobil" placeholder="Masukan merek mobil" required>
+            </b-form-input>
+          </b-form-group>
+          <b-form-group id="input-group-2" label="Harga:" label-for="input-2">
+            <b-form-input v-model="form_edit.tipe_mobil" value="form.tipe_mobil" placeholder="Masukan tipe mobil" required>
+            </b-form-input>
+          </b-form-group>
+          <b-form-group id="input-group-3" label="kategori:" label-for="input-3">
+            <b-form-input v-model="form_edit.warna" value="form.warna" placeholder="Masukan warna mobil" required>
+            </b-form-input>
+          </b-form-group>
+            <b-button type="submit" variant="primary">Submit</b-button>
+        </b-form>
+      </div>
+      <!-- end form edit -->
+
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+    data() {
+      return {
+        form: {
+          merek_mobil: "",
+          tipe_mobil: "",
+          warna: "",
+        },
+        onEdit: false,
+        fields: [
+          { key: "id", sortable: "true" },
+          { key: "merek_mobil", sortable: "true" },
+          { key: "tipe_mobil", sortable: "true" },
+          { key: "warna", sortable: "true" },
+          { key: "#", sortable: "true" },
+        ],
+        items: [],
+      };
+    },
+    methods: {
+        async getapi() {
+        const data = await this.$axios.$get("http://localhost:35000/users/car");
+        this.items = data.data;
+        for(let i = 0; i < this.items.length; i++){
+          const element = this.items[i]
+          element.id = i + 1
+        }
+        console.log(data.data);
+      },
+      async onDelete(data) {
+        await this.$axios.$delete(
+          `http://localhost:35000/users/car/` + data.id
+        );
+        this.getapi();
+      },
+      async onSubmit(event) {
+        event.preventDefault();
+        try {
+          await this.$axios.$post(
+            "http://localhost:35000/users/car",
+            this.form
+          );
+          this.getapi();
+        } catch (error) {
+          console.log(error);
+        }
+      },
+      async onReset(event) {
+        event.preventDefault();
+        this.form.merek_mobil = "";
+        this.form.tipe_mobil = "";
+        this.form.warna = "";
+        this.show = false;
+        this.$nextTick(() => {
+          this.show = true;
+        });
+      },
+      async onEditForm(data) {
+        this.onEdit = true; 
+        this.form_edit.merek_mobil = data.merek_mobil;
+        this.form_edit.tipe_mobil = data.tipe_mobil;
+        this.form_edit.warna = data.warna;
+        this.form_edit.id = data.id;
+      },
+      async onSubmitEdit(event) {
+        event.preventDefault();
+        try{
+            await this.$axios.$put(
+              `http://localhost:35000/users/car/` + this.form_edit.id,
+              this.form_edit
+            );
+            this.getapi();
+        }
+        catch(error){
+          console.log(error);
+        }
+      },
+    },
+    mounted() {
+      this.getapi();
+    }
+  }
+</script>
